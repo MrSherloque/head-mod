efeca3e03841047035351c00086ace74c22241a7
Simon <simon@stausholm.info>
2023-04-09 22:33:45 +0200

    Fabric Item

--- src/main/java/net/head/mod/heads.java
+++ src/main/java/net/head/mod/heads.java
@@ -1,12 +1,18 @@
 package net.head.mod;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.minecraft.item.Item;
 
 public class heads implements ModInitializer {
 
+    public static final Item FABRIC_ITEM = new Item(new Item.Settings().group(ItemGroup.MISC));
+
 	@Override
 	public void onInitialize() {
-        
+        Registry.register(Registry.ITEM, new Identifier("heads", "fabric_item"), FABRIC_ITEM);
 	}
 
 }
